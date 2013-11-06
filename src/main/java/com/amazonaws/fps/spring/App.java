2d5a324ffa74cacd93aee1958c72903023ab1f8e
aweiland <aweiland@gmail.com>
2013-11-05 22:00:53 -0500

    Made repo

---
+++ src/main/java/com/amazonaws/fps/spring/App.java
@@ -0,0 +1,13 @@
+package com.amazonaws.fps.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
